63d79153bde7a145b6b49303a50f8ea381e374d6
Katrice White <kwhite@chicagotechacademy.org>
2013-12-03 16:27:26 -0600

    Second Test Pass!!!

--- lib/fizzbuzz12.rb
+++ lib/fizzbuzz12.rb
@@ -1,5 +1,11 @@
 	class Calculator
 		def calculate(num)
-		1
+			if num == 1
+				return 1
+			elsif num == 2
+				return 2
+			else 
+				return "fail"
+			end	
 		end
 	end
--- spec/fizzbuzz12_spec.rb
+++ spec/fizzbuzz12_spec.rb
@@ -8,5 +8,9 @@ describe 'fizzbuzz calculator' do
 	it 'returns 1 from 1' do 
 	    @calculator.calculate(1).should == 1
 	end
+
+	it 'returns 2 from 2' do
+		@calculator.calculate(2).should == 2
+	end
 	
 end
